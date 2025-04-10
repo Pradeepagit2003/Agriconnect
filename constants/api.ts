7a5b66a3e88d654f7c6550e72f0a70c750208567
poojaelangovan516 <poojaelangovan516@gmail.com>
2025-04-10 08:42:33 +0530

    Update API and dependencies

--- constants/api.ts
+++ constants/api.ts
@@ -1,7 +1,7 @@
 // export const baseUrl = "http://192.168.1.5:8081/"
-export const baseUrl = "http://192.168.1.7:8080/"
+export const baseUrl = "http://192.168.111.249:8080/"
 // export const baseUrl = "http://localhost:8080/"
 
 export const getbaseUrl  = (port:number) => {
-    return `http://192.168.1.5:${port}/`
+    return `http://192.168.111.249:${port}/`
 }
